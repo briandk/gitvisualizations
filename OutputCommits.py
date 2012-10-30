e6639cacc3295e6e00e1b237b397e6279c0e7e39
Brian Danielak <briandk@umd.edu>
2012-10-29 23:36:38 -0700

    Re-indent openOutputFile()

--- OutputCommits.py
+++ OutputCommits.py
@@ -6,15 +6,15 @@ from git import *
 
 class GitTimeline(dict):
     def __init__(self):
-        dict.__init__(self)
-        file = '/Users/briandanielak/Dropbox/dev/roxygen/DESCRIPTION' # replace with sys.argv[1]
-        self['repo'] = Repo(file)
-        self['fileRevisions'] = self['repo'].git.log(file, format='%H').splitlines()
-        self['blames'] = [self['repo'].git.blame(revision, '--root', '--show-number', '-s', file).splitlines()
-                                            for revision in self['fileRevisions']]
+    dict.__init__(self)
+    file = '/Users/briandanielak/Dropbox/dev/roxygen/DESCRIPTION' # replace with sys.argv[1]
+    self['repo'] = Repo(file)
+    self['fileRevisions'] = self['repo'].git.log(file, format='%H').splitlines()
+    self['blames'] = [self['repo'].git.blame(revision, '--root', '--show-number', '-s', file).splitlines()
+                        for revision in self['fileRevisions']]
 
-        self['css'] = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r')
-        self['output'] =
+    self['css'] = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r')
+    self['output'] =
 
 def outputCommits(
     repositoryPath = '/Users/briandanielak/Dropbox/dev/roxygen',
@@ -45,17 +45,17 @@ def writeBlames(blames):
     return None
 
 def openOutputFile(pathname):
-        pathname = os.path.expanduser(pathname)
-        if os.path.isfile(pathname):
-                try:
-                        f = open(pathname, 'w')
-                        f.write('')
-                        f.close()
-                except IOError:
-                        pass
+    pathname = os.path.expanduser(pathname)
+    if os.path.isfile(pathname):
+        try:
+            f = open(pathname, 'w')
+            f.write('')
+            f.close()
+        except IOError:
+            pass
 
-        f = open(pathname, 'a')
-        return f
+    f = open(pathname, 'a')
+    return f
 
 
 if __name__ == '__main__':
