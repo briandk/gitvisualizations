52ee681b0c7ad575c37a1d8706b28639d41bd856
Brian Danielak <briandk@umd.edu>
2012-10-29 23:34:33 -0700

    Add css entry to GitTimeline class
    Change indentation to 4 spaces per level

--- OutputCommits.py
+++ OutputCommits.py
@@ -11,64 +11,63 @@ class GitTimeline(dict):
         self['repo'] = Repo(file)
         self['fileRevisions'] = self['repo'].git.log(file, format='%H').splitlines()
         self['blames'] = [self['repo'].git.blame(revision, '--root', '--show-number', '-s', file).splitlines()
-                          for revision in self['fileRevisions']]
+                                            for revision in self['fileRevisions']]
+
+        self['css'] = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r')
+        self['output'] =
 
 def outputCommits(
-  repositoryPath = '/Users/briandanielak/Dropbox/dev/roxygen',
-  filename = 'DESCRIPTION'
-  ):
-  '''Creates an HTML timeline
-  of all a file's revisions.
-  '''
+    repositoryPath = '/Users/briandanielak/Dropbox/dev/roxygen',
+    filename = 'DESCRIPTION'
+    ):
+    '''Creates an HTML timeline
+    of all a file's revisions.
+    '''
 
-  t = GitTimeline()
-  print t.keys()
-  # revisions = getHashesOfFileCommits(repo, filename)
-  # blames = getBlames(repo, revisions)
-  # pdb.set_trace()
-  # writeTimeline(revisions, blames)
+    t = GitTimeline()
+    print t['css'].read()
+    # revisions = getHashesOfFileCommits(repo, filename)
+    # blames = getBlames(repo, revisions)
+    # pdb.set_trace()
+    # writeTimeline(revisions, blames)
 
-  return None
+    return None
 
 def checkArguments(inputFile, outputFile):
-  '''
-  if inputFile is None or !(git repo), throw exception
-  if outputFile is None, throw a helpful exception
-  '''
-
-def getBlames(repo, revisions):
-  return (
-    [repo.git.blame(revision, '--root', '--show-number', '-s', 'DESCRIPTION').splitlines() for revision in revisions]
-  )
+    '''
+    if inputFile is None or !(git repo), throw exception
+    if outputFile is None, throw a helpful exception
+    '''
 
 def writeBlames(blames):
-  print '\n'.join(blames[0])
+    print '\n'.join(blames[0])
 
-  return None
+    return None
 
 def openOutputFile(pathname):
-    pathname = os.path.expanduser(pathname)
-    if os.path.isfile(pathname):
-        try:
-            f = open(pathname, 'w')
-            f.write('')
-            f.close()
-        except IOError:
-            pass
+        pathname = os.path.expanduser(pathname)
+        if os.path.isfile(pathname):
+                try:
+                        f = open(pathname, 'w')
+                        f.write('')
+                        f.close()
+                except IOError:
+                        pass
 
-    f = open(pathname, 'a')
-    return f
+        f = open(pathname, 'a')
+        return f
 
 
 if __name__ == '__main__':
-  outputCommits()
+    outputCommits()
+
 
 def writeTimeline(revisions, blames, outputFile='~/gitdatacollection/aaa.html'):
-  css = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r')
-  output =  openOutputFile(outputFile)
+    css = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r')
+    output =  openOutputFile(outputFile)
 
-  print css
-  return None
+    print css
+    return None
 
 
 
