fc5633e9943f09b5161017144c660a959cce6e90
Brian Danielak <briandk@umd.edu>
2012-10-30 17:38:02 -0700

    Refactor GitTimeline class to use properties
    Use bare properties for repo, fileRevisions, css
    Use decorated properties to handle input and output files

--- OutputCommits.py
+++ OutputCommits.py
@@ -4,13 +4,21 @@ import time
 
 from git import *
 
-class GitTimeline(dict):
+class GitTimeline(object):
     def __init__(self):
         file = '/Users/briandanielak/Desktop/testrepo/test.txt' # replace with sys.argv[1]
-        self['repo'] = Repo(file)
-        self['fileRevisions'] = self['repo'].git.log(file, format='%h').splitlines()
-        self['css'] = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r').read()
-        self['output'] = ''
+        self.repo = Repo(file)
+        self.fileRevisions = self.repo.git.log(file, format='%h').splitlines()
+        self.css = open(os.path.normpath('%s/../TimelineStyle.css' % sys.argv[0]), 'r').read()
+        self.files = dict()
+
+    @property
+    def input(self):
+        return self.files['input']
+
+    @input.setter
+    def input(self, value):
+        self.files['input'] = value
 
     def openOutputFile(self, pathname='~/gitdatacollection/foo.html'):
         pathname = os.path.expanduser(pathname)
@@ -22,34 +30,31 @@ class GitTimeline(dict):
             except IOError:
                 pass
 
-        self['output'] = open(pathname, 'a')
+        self.output = open(pathname, 'a')
         return self
 
     def writeTimeline(self):
-        self['fileRevisions'].reverse()
-        self['output'].write('<html><head>\n%s\n</head><body><table><tr>\n' % self['css'])
-        [self.writeBlame(revision) for revision in self['fileRevisions']]
+        self.fileRevisions.reverse()
+        self.output.write('<html><head>\n%s\n</head><body><table><tr>\n' % self.css)
+        [self.writeBlame(revision) for revision in self.fileRevisions]
 
         return None
 
     def writeBlame(self, revision, file='/Users/briandanielak/Desktop/testrepo/test.txt'):
-        blame = self['repo'].git.blame(revision, '--root', '--show-number', '-s', file).splitlines()
+        blame = self.repo.git.blame(revision, '--root', '--show-number', '-s', file).splitlines()
         blame = [line.startswith(revision) and '<span class="changed">%s</span>' % line or line
                     for line in blame]
-        timestamp = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime(self['repo'].commit(revision).committed_date))
-        self['output'].write('<td><a class=timestamp, name=%s>%s</a><br />' % (revision, timestamp))
-        self['output'].write('<pre>%s</pre></td>' % '\n'.join(blame))
+        timestamp = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime(self.repo.commit(revision).committed_date))
+        self.output.write('<td><a class=timestamp, name=%s>%s</a><br />' % (revision, timestamp))
+        self.output.write('<pre>%s</pre></td>' % '\n'.join(blame))
         return None
 
     def closeFiles(self):
-        [v.close() for v in self.values() if type(v) is file]
+        [v.close() for v in self.files.values() if type(v) is file]
         return None
 
 
-def outputCommits(
-    repositoryPath = '/Users/briandanielak/Dropbox/dev/roxygen',
-    filename = 'DESCRIPTION'
-    ):
+def outputCommits():
     '''Creates an HTML timeline
     of all a file's revisions.
     '''
