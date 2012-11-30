24073e64788831556a936d99379c4bf3226b8594
Brian Danielak <briandk@umd.edu>
2012-11-29 22:40:07 -0500

    Add basic function for scrolling to a commit from search
    Bug: Upon hitting "Go!", the document will scroll, but after several
    seconds will return to the root commit.

--- Timeline.js
+++ Timeline.js
@@ -2,10 +2,17 @@
 $('#shaSearch').typeahead( {source: revisions} );
 
 var timeline = {};
+timeline.counter = 0;
+timeline.revisions = revisions;
 timeline.toggleBtn = function () { $(this).toggleClass("active") };
-timeline.goToCommit = function(commitHash) {
-  destinationOffset = $('#' + commitHash).offset().left
+timeline.goToCommit = function(sha) {
+  var destinationOffset = $('#' + sha).offset().left
   $(window).scrollLeft(destinationOffset)
+};
+timeline.navigateToRevisionFromSearch = function () {
+  var sha = $('#shaSearch').val()
+  timeline.goToCommit(sha)
 }
 
 $(".toggleable").bind('click', timeline.toggleBtn);
+$("#goToSha").bind('click', timeline.navigateToRevisionFromSearch)
