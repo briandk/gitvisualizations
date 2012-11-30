b8a4d51ccc93e543295e35bf5185b81ef41b7fca
Brian Danielak <briandk@umd.edu>
2012-11-29 23:27:43 -0500

    Scroll to commit on SHA search form submit
    Closes #27

--- Timeline.js
+++ Timeline.js
@@ -6,13 +6,14 @@ timeline.counter = 0;
 timeline.revisions = revisions;
 timeline.toggleBtn = function () { $(this).toggleClass("active") };
 timeline.goToCommit = function(sha) {
-  var destinationOffset = $('#' + sha).offset().left
-  $(window).scrollLeft(destinationOffset)
+  var destinationOffset = $('#' + sha).offset().left;
+  $("body").animate({scrollLeft: destinationOffset}, 400);
 };
 timeline.navigateToRevisionFromSearch = function () {
-  var sha = $('#shaSearch').val()
-  timeline.goToCommit(sha)
+  var sha = $('#shaSearch').val();
+  timeline.goToCommit(sha);
 }
 
 $(".toggleable").bind('click', timeline.toggleBtn);
-$("#goToSha").bind('click', timeline.navigateToRevisionFromSearch)
+$("#shaForm").on('submit', function () {return(false)});
+$("#goToSha").on('click', timeline.navigateToRevisionFromSearch);
