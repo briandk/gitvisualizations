7bc9bd3920d9880b2e0f93a1d63ca013e9a4bf24
Brian Danielak <briandk@umd.edu>
2013-01-21 17:26:14 -0500

    Add parser to munge log data
    returns a list of objects (hashes), each of which contains the
    datestamp and a list of changed files and their diffstat information.

--- tests/LogLineParser.py
+++ tests/LogLineParser.py
@@ -1,8 +1,31 @@
-import os
-print os.getcwd()
-with open("loglines.txt") as f:
-    lines = f.read().splitlines()
-    print lines
+# global variables (for now)
+logfile = "loglines.txt"
+datetime_indicator = ' '
 
-lines = [line for line in lines if line is not '']
-print lines
+loglet = {"header": '', "content": []}
+output = []
+
+with open(logfile) as f:
+    currentLine = f.readline()
+    while f.readline():
+        if currentLine.startswith('\n'):
+            print ("line is blank")
+            output.append(loglet)
+            loglet['header'] = ''
+            loglet['content'] = []
+        elif currentLine.startswith(datetime_indicator):
+            print "entering if branch"
+            print currentLine
+            loglet['header'] = currentLine
+        else:
+            print "entering else branch"
+            print currentLine
+            loglet['content'].append(currentLine)
+        currentLine = f.readline()
+    output.append(loglet)
+
+print output[0]['content']
+print output[1]['header']
+
+
+
