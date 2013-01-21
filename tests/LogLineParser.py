d89ab8f985b33abb8fe00cfdfbec72b2873dcf2d
Brian Danielak <briandk@umd.edu>
2013-01-21 17:30:28 -0500

    Remove print statements from logline parser

--- tests/LogLineParser.py
+++ tests/LogLineParser.py
@@ -1,31 +1,25 @@
 # global variables (for now)
 logfile = "loglines.txt"
 datetime_indicator = ' '
+blank_line_indicator = '\n'
 
-loglet = {"header": '', "content": []}
+loglet = {"header": '',
+          "content": []}
 output = []
 
 with open(logfile) as f:
     currentLine = f.readline()
     while f.readline():
-        if currentLine.startswith('\n'):
-            print ("line is blank")
+        if currentLine.startswith(blank_line_indicator):
             output.append(loglet)
             loglet['header'] = ''
             loglet['content'] = []
         elif currentLine.startswith(datetime_indicator):
-            print "entering if branch"
-            print currentLine
             loglet['header'] = currentLine
         else:
-            print "entering else branch"
-            print currentLine
             loglet['content'].append(currentLine)
         currentLine = f.readline()
     output.append(loglet)
-
-print output[0]['content']
-print output[1]['header']
 
 
 
