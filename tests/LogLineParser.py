d78f9c8f03d5ea03ccbd0f4e7ee407d00a7e220d
Brian Danielak <briandk@umd.edu>
2013-01-21 17:40:34 -0500

    wrap logline parsing behavior in parse_log_lines()

--- tests/LogLineParser.py
+++ tests/LogLineParser.py
@@ -1,25 +1,25 @@
 # global variables (for now)
-logfile = "loglines.txt"
-datetime_indicator = ' '
-blank_line_indicator = '\n'
-
-loglet = {"header": '',
-          "content": []}
-output = []
-
-with open(logfile) as f:
-    currentLine = f.readline()
-    while f.readline():
-        if currentLine.startswith(blank_line_indicator):
-            output.append(loglet)
-            loglet['header'] = ''
-            loglet['content'] = []
-        elif currentLine.startswith(datetime_indicator):
-            loglet['header'] = currentLine
-        else:
-            loglet['content'].append(currentLine)
+def parse_log_lines(logfile):
+    datetime_indicator = ' '
+    blank_line_indicator = '\n'
+    loglet = {"header": '',
+              "content": []}
+    output = []
+    with open(logfile) as f:
         currentLine = f.readline()
-    output.append(loglet)
+        while f.readline():
+            if currentLine.startswith(blank_line_indicator):
+                output.append(loglet)
+                loglet['header'] = ''
+                loglet['content'] = []
+            elif currentLine.startswith(datetime_indicator):
+                loglet['header'] = currentLine
+            else:
+                loglet['content'].append(currentLine)
+            currentLine = f.readline()
+        output.append(loglet)
+    return output
+
 
 
 
