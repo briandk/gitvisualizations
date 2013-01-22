4f14d5777e9d0316780d8ab1ba2e83a7d92b20bc
Brian Danielak <briandk@umd.edu>
2013-01-21 19:54:40 -0500

    Add skeleton for basic shell control script
    Outline getting a log-file, processing it into a CSV, then sending it
    to R.

---
+++ RepoStatistics/RepoStatistics.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+# To generate repo statistics
+#   change to the directory of interest
+#   make temporary log output file
+#   make temporary CSV output file
+#   git log > temporary file
+#   python templog > tempcsv
+#   R csv
