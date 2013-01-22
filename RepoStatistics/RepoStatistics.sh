1582c34bc8459566e7d7d83d6995214c180c948a
Brian Danielak <briandk@umd.edu>
2013-01-21 20:12:19 -0500

    Have script create and populate the git log for a repo

--- RepoStatistics/RepoStatistics.sh
+++ RepoStatistics/RepoStatistics.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/bash
+# Takes a single argument: the path to a repository
+
+cd ${1}
+TEMPLOG=$(mktemp /tmp/output.XXXXXXXXXX)
+TMPCSV=$(mktemp /tmp/output.XXXXXXXXXX)
+git log --numstat --date=iso --format=' %H,%ad' > $TEMPLOG
 
 # To generate repo statistics
-#   change to the directory of interest
-#   make temporary log output file
-#   make temporary CSV output file
-#   git log > temporary file
+#   X change to the directory of interest
+#   X make temporary log output file
+#   X make temporary CSV output file
+#   X git log > temporary file
 #   python templog > tempcsv
 #   R csv
+
