9aee9df455e243263df12584b5b113ea382a5d7b
Brian Danielak <briandk@umd.edu>
2013-01-27 19:23:09 -0500

    Make GitLogCaller class

--- RepoStatistics/LogLineParser.py
+++ RepoStatistics/LogLineParser.py
@@ -1,39 +1,48 @@
 import argparse
 import subprocess
 
-def create_git_log_call():
-    command_line_arguments = ["--%s=%s" % (k,v)
-                                  for k,v
-                                  in vars(parseCommandLineArguments()).iteritems()
-                                  if v is not None]
-    print command_line_arguments
+class GitLogCaller(object):
+    def __init__(self):
+        self.args = self.parseCommandLineArguments()
+        self.git_log_call = ["git", "log", "--numstat", "--date=iso", "--format=' %H,%ad'"]
+        self.git_log_call.extend(self.create_git_log_call())
+        for item in self.git_log_call:
+            print item
 
-def parseCommandLineArguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("repo_path", help="Path to the repo of interest")
-    parser.add_argument("--since", help="the beginning of the time interval of interest")
-    parser.add_argument("--until", help="the endpoint of the time interval of interest")
-    return parser.parse_args()
+    def create_git_log_call(self):
+        optional_arguments = ["since", "until"]
+        command_line_arguments = ["--%s='%s'" % (k,v)
+                                      for k,v
+                                      in vars(self.args).iteritems()
+                                      if k in optional_arguments and v is not None]
+        return command_line_arguments
 
-def parse_log_lines(logfile):
-    datetime_indicator = ' '
-    blank_line_indicator = '\n'
-    loglet = {"header": '',
-              "content": []}
-    output = []
-    with open(logfile) as f:
-        currentLine = f.readline()
-        while f.readline():
-            if currentLine.startswith(blank_line_indicator):
-                output.append(loglet)
-                loglet['header'] = ''
-                loglet['content'] = []
-            elif currentLine.startswith(datetime_indicator):
-                loglet['header'] = currentLine
-            else:
-                loglet['content'].append(currentLine)
+    def parseCommandLineArguments(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("repo_path", help="Path to the repo of interest")
+        parser.add_argument("--since", help="the beginning of the time interval of interest")
+        parser.add_argument("--until", help="the endpoint of the time interval of interest")
+        return parser.parse_args()
+
+    def parse_log_lines(self):
+        datetime_indicator = ' '
+        blank_line_indicator = '\n'
+        loglet = {"header": '',
+                  "content": []}
+        output = []
+        with open(logfile) as f:
             currentLine = f.readline()
-        output.append(loglet)
-    return output
+            while f.readline():
+                if currentLine.startswith(blank_line_indicator):
+                    output.append(loglet)
+                    loglet['header'] = ''
+                    loglet['content'] = []
+                elif currentLine.startswith(datetime_indicator):
+                    loglet['header'] = currentLine
+                else:
+                    loglet['content'].append(currentLine)
+                currentLine = f.readline()
+            output.append(loglet)
+        return output
 
-create_git_log_call()
+g = GitLogCaller()
