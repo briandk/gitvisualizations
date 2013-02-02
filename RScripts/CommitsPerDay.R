7e18fb565a5171aa68243b186f03401ab03b967b
Brian Danielak <briandk@umd.edu>
2013-02-02 16:56:57 -0500

    Refactor formatDatesAndTimes()

--- RScripts/CommitsPerDay.R
+++ RScripts/CommitsPerDay.R
@@ -9,13 +9,15 @@ csv.file = "~/Dropbox/dev/granovaGG/repo_statistics.csv"
 repo.statistics <- read.csv(csv.file, stringsAsFactors=FALSE)
 
 formatDatesAndTimes <- function(repo.statistics) {
-  repo.statistics$date <- ymd(repo.statistics$date)
-  repo.statistics$time <- hms(repo.statistics$time)
-  repo.statistics$datetime <- ymd_hms(repo.statistics$datetime)
-  return(repo.statistics)
+  output <- within(repo.statistics, {
+    date <- ymd(date)
+    time <- hms(time)
+    datetime <- ymd_hms(datetime)
+  })
+  return(output)
 }
 
-formatAdditionsAndDeletions <- function()
+# formatAdditionsAndDeletions <- function()
 
 repo.statistics <- formatDatesAndTimes(repo.statistics)
 str(repo.statistics)
