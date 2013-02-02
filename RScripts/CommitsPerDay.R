504bdb4f26368ea603590decf0640b767ee48202
Brian Danielak <briandk@umd.edu>
2013-02-02 16:45:33 -0500

    Format dates and times for R analysis

--- RScripts/CommitsPerDay.R
+++ RScripts/CommitsPerDay.R
@@ -1,16 +1,21 @@
 # TODO: use ddply to summarize the data so I can plot raw counts
-# Export several CSVs from the additions/deletions data, then merge()
 
 library(ggplot2)
 library(plyr)
 library(lubridate)
-commits <- read.csv("~/Dropbox/2013Spring/PracticePub/RebeccasProject2Commits.csv",
-                    stringsAsFactors=FALSE)
-convertToDates <- function (x) {
-  return(ymd_hms(x))
+
+csv.file = "~/Dropbox/dev/granovaGG/repo_statistics.csv"
+
+repo.statistics <- read.csv(csv.file, stringsAsFactors=FALSE)
+
+formatDatesAndTimes <- function(repo.statistics) {
+  repo.statistics$date <- ymd(repo.statistics$date)
+  repo.statistics$time <- hms(repo.statistics$time)
+  repo.statistics$datetime <- ymd_hms(repo.statistics$datetime)
+  return(repo.statistics)
 }
 
-commits$commit <- convertToDates(commits$commit)
-str(commits)
-p <- ggplot(aes(x = commit), data=commits)
-p + geom_line() + geom_density(aes(y = ..scaled..))
+formatAdditionsAndDeletions <- function()
+
+repo.statistics <- formatDatesAndTimes(repo.statistics)
+str(repo.statistics)
