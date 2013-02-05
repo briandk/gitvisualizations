371bdddb051eec81d9561944fdc51228bcaa57fc
Brian Danielak <briandk@umd.edu>
2013-02-04 21:30:13 -0500

    Plot small multiple summary of lines added/deleted by day

--- RScripts/CommitsPerDay.R
+++ RScripts/CommitsPerDay.R
@@ -34,7 +34,7 @@ getCommitsPerDay <- function(repo.statistics) {
                   summarize,
                   commits.per.day = length(lines_added)
             )
-  output <- transform(output, 
+  output <- transform(output,
                       cumulative.commits = cumsum(commits.per.day)
             )
   return(output)
@@ -42,7 +42,7 @@ getCommitsPerDay <- function(repo.statistics) {
 
 plotCommitsPerDay <- function(repo.statistics) {
   p <- ggplot(
-            aes(x = date, 
+            aes(x = date,
                 y = commits.per.day),
             data = getCommitsPerDay(repo.statistics))
   p <- p + geom_line()
@@ -61,8 +61,55 @@ cumulativeCommitsPerDay <- function() {
   )
 }
 
+getLinesAddedAndLinesDeletedByDay <- function(repo.statistics) {
+  output <- ddply(repo.statistics,
+                  .(date, filename),
+                  summarize,
+                  lines_added = sum(lines_added),
+                  lines_deleted = sum(-lines_deleted)
+            )
+  return(output)
+}
+
+SummaryOfLinesAddedAndLinesDeletedByDay <- function(repo.statistics) {
+  p <- ggplot(data = getLinesAddedAndLinesDeletedByDay(repo.statistics))
+  p <- p + linesAddedByDay()
+  p <- p + linesDeletedByDay()
+  p <- p + theme(axis.text.x = element_text(angle = 90))
+  p <- p + ggtitle("Summary of Lines Added/Deleted Over Time")
+  p <- p + xlab("Date")
+  p <- p + ylab("Lines Added/Deleted")
+  p <- p + facet_wrap(~ filename)
+  return(p)
+}
+
+linesAddedByDay <- function() {
+  return(
+    geom_area(
+      aes(x = date,
+          y = lines_added
+      ),
+      fill = "darkblue",
+      alpha = 0.5
+    )
+  )
+}
+
+linesDeletedByDay <- function() {
+  return(
+    geom_area(
+      aes(x = date,
+          y = lines_deleted
+      ),
+      fill = "orange",
+      alpha = 0.5
+    )
+  )
+}
+
 repo.statistics <- formatDatesAndTimes(repo.statistics)
 repo.statistics <- formatAdditionsAndDeletions(repo.statistics)
 p <- plotCommitsPerDay(repo.statistics)
 print(p)
-
+p <- SummaryOfLinesAddedAndLinesDeletedByDay(repo.statistics)
+print(p)
